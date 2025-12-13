82e486e7d92bfa26b0f29c8ad27e7ce2c044ac03
fu2futu <zjinbei031@gmail.com>
2025-12-13 17:23:07 +0900

    Add guard to double()
    機能追加

--- double.c
+++ double.c
@@ -4,6 +4,12 @@ int double(int x){
     return x * 2;
 }
 
+int double(int x){
+    if (x > 1000000 || x < -1000000) return 0;
+    return x * 2;
+}
+
+
 int main(void){
     int a;
     if (scanf("%d", &a) != 1) return 1;
