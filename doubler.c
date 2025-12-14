0130561d2f7a3b055b9e8e613f5f6bb48790ae34
fu2futu <zjinbei031@gmail.com>
2025-12-14 14:25:37 +0900

    入力欄

--- doubler.c
+++ doubler.c
@@ -7,6 +7,12 @@ int doubler(int x){
 
 int main(void){
     int a;
+
+    printf("ひなた\n");
+    printf("ここに名前\n");
+    printf("ここに名前\n");
+    printf("ここに名前\n");
+
     if (scanf("%d", &a) != 1) return 1;
     printf("%d\n", doubler(a));
     return 0;
